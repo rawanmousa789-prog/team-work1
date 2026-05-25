8dfe08912c1b6990336fd9bcc62834005fa4bc6b
rawan <rawanmousa366@gmail.com>
2026-05-25 13:44:50 +0300

    edit on the registor page

--- js/login.js
+++ js/login.js
@@ -1,11 +1,10 @@
 var togglePassword = document.getElementById('togglePassword');
-        var passwordInput = document.getElementById('passwordInput');
+var passwordInput = document.getElementById('passwordInput');
 
-        togglePassword.addEventListener('click', function () {
-            var type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-            passwordInput.setAttribute('type', type);
-            
-            var icon = this.querySelector('i');
-            icon.classList.toggle('fa-eye');
-            icon.classList.toggle('fa-eye-slash');
-        });
+togglePassword.addEventListener('click', function () {
+    var type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);    
+    var icon = this.querySelector('i');
+    icon.classList.toggle('fa-eye');
+    icon.classList.toggle('fa-eye-slash');
+});
--- js/register.js
+++ js/register.js
@@ -1,13 +1,26 @@
-function toggleRegPass(inputId, el) {
+const togglePassword = document.getElementById('togglePassword');
+const passwordInput = document.getElementById('passwordInput');
 
-    var input = document.getElementById(inputId);
-    var icon = el.querySelector('i');
-    
-    if (input.type === "password") {
-        input.type = "text";
-        icon.classList.replace("fa-eye", "fa-eye-slash");
-    } else {
-        input.type = "password";
-        icon.classList.replace("fa-eye-slash", "fa-eye");
-    }
+if(togglePassword && passwordInput){
+    togglePassword.addEventListener('click' , ()=>{
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' :'password';
+        passwordInput.setAttribute('type' , type);
+
+        const icon = togglePassword.querySelector('i');
+        icon.classList.toggle('fa-eye');
+        icon.classList.toggle('fa-eye-slash');
+    });
+}
+    const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+    const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+
+if (toggleConfirmPassword && confirmPasswordInput) {
+    toggleConfirmPassword.addEventListener('click', () =>{
+        const type = confirmPasswordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        confirmPasswordInput.setAttribute('type', type);
+
+        const icon = toggleConfirmPassword.querySelector('i');
+        icon.classList.toggle('fa-eye');
+        icon.classList.toggle('fa-eye-slash');
+    });
 }
